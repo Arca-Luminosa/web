f12cb16ab2a6ee1e3e71ef18420817bc3e8840f5
Carlos Sosa <cesosag@gmail.com>
2024-08-14 21:03:03 +0100

    Add SupaBase storage support

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -1,7 +1,31 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': [`'self'`],
+          'img-src': [
+            `'self'`,
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('SUPABASE_API_URL'),
+          ],
+          'media-src': [
+            `'self'`,
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('SUPABASE_API_URL'),
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-upload-supabase-provider',
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_BUCKET_NAME'),
+        directory: env('SUPABASE_BUCKET_DIRECTORY'),
+      }
+    },
+  },
+});
