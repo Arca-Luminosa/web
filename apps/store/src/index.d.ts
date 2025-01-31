5d9f4e4351080bcb3bf4880967796d54fd78a5aa
Carlos Sosa <cesosag@gmail.com>
2025-01-31 17:49:15 +0000

    Clean old code

--- apps/store/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare module '@medusajs/medusa/dist/models/customer' {
-	declare interface Customer {
-		payment_methods: WompiPaymentMethod[]
-	}
-}
--- apps/store/src/models/customer.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, OneToMany } from 'typeorm'
-import { Customer as MedusaCustomer } from '@medusajs/medusa'
-import { WompiPaymentMethod } from './wompiPaymentMethod'
-
-@Entity()
-export class Customer extends MedusaCustomer {
-  @OneToMany(() => WompiPaymentMethod, (paymentMethods) => paymentMethods)
-  payment_methods: WompiPaymentMethod[]
-}
